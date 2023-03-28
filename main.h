d337cdf5a22b8b2d02705293a7f4aa7af32c3848
Smlchinaza <samchuks898@gmail.com>
2023-03-28 14:18:23 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int _putchar(char c);
+
+#endif
